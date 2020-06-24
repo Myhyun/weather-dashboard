d42cb4474259e41432dc7e783618f6096f9909a1
Myhyun <mattyihyun@gmail.com>
2020-06-24 10:16:05 -0700

    correctly displaying current weather conditions for inputted city

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 var apiKey = "7563abe66b400c1ddd2f278ccaf0967b";
+var mainWeather = $(".current-weather");
+
 
 $("#search-button").on("click", function () {
-    var cityName = $("#cityname").val();
-    console.log(cityName);
+    var search = $("#cityname").val();
+    console.log(search);
 
 
 
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&units=imperial&appid=" + apiKey;
     console.log(queryURL);
 
 
     $.ajax({
         url: queryURL,
         method: "GET",
-    }).then(function (response){
-        var results = response.data
-        
-        for (var i=0; i < results.length; i++) {
-            var displayDiv = $("<div>");
-            var temp = results[i].list[i].main.temp;
-            var tempP = $("<p>").text("Temperature: " + temp);
-            console.log(tempP);
-            console.log(displayDiv);
+    }).then(function (response) {
+        console.log(response);
+        var mainWeather = {
+            cityName: response.city.name,
+            date: response.list[0].dt_txt,
+            humidity: response.list[0].main.humidity,
+            windSpeed: response.list[0].wind.speed,
+            temp: response.list[0].main.temp
         }
+
+        var currentCity = $("<p>");
+        currentCity.text(JSON.stringify(mainWeather.cityName) + JSON.stringify(mainWeather.date));
+        $(".displaycurrentWeather").append(currentCity);
+        var currentTemp = $("<p>");
+        currentTemp.text("Temperature: " + JSON.stringify(mainWeather.temp) + "  °F");
+        $(".displaycurrentWeather").append(currentTemp);
+        var currentHumidity = $("<p>");
+        currentHumidity.text("Humidity: " + JSON.stringify(mainWeather.humidity) + " %");
+        $(".displaycurrentWeather").append(currentHumidity);
+        var currentWindspeed = $("<p>");
+        currentWindspeed.text("Wind Speed: " + JSON.stringify(mainWeather.windSpeed) + " mph");
+        $(".displaycurrentWeather").append(currentWindspeed);
+
+
+
+
+
 
 
     });
 });
-
-
-
-
-
-
